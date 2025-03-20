c4c6c9b2c1a37c9728033ae9a058faf9f5c4786c
Tim Knapp <timknapp12@gmail.com>
2025-03-20 01:23:33 -0600

    make card space-between

--- packages/ui/src/components/Card.tsx
+++ packages/ui/src/components/Card.tsx
@@ -1,6 +1,6 @@
 export const Card = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='bg-gray-100 p-4 rounded-md w-full min-h-[200px] flex flex-col gap-4'>
+    <div className='bg-gray-100 p-4 rounded-md w-full flex flex-col gap-4 justify-between'>
       {children}
     </div>
   );
