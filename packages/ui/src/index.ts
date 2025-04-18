b8890fc807e14ece941a5a4d32b5591440a6b66a
Tim Knapp <timknapp12@gmail.com>
2025-04-18 11:48:22 -0600

    add Tabs UI

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,4 +1,5 @@
 export * from './components/Button';
 export * from './components/Select';
 export * from './components/Card';
+export * from './components/Tabs';
 // Export other components as they're added
